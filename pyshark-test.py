077030b83a5f077b8d2846b432c3c74e8edae7cc
GrillednRoasted <sferrer2001@gmail.com>
2022-07-10 18:32:58 -0700

    added online ip database check

--- pyshark-test.py
+++ pyshark-test.py
@@ -4,10 +4,27 @@ import pyshark
 import time 
 import subprocess as sp
 import os
-
+import request as req
+import json
+import socket
 capture = pyshark.LiveCapture(interface='wlo1',output_file="test.pcap")
 separator = '------------------------------------------------------------------------'
 pcap_file = "test.pcap"
+malicious_ip = []
+
+def check_ip( ip):
+
+    with open("key") as key:
+        key = key.read().strip()
+
+    data = {"maxAgeInDays":90}
+    headers = {"Key":key,"Accept":"application/json"}
+
+    url = f"https://api.abuseipdb.com/api/v2/check?ipAddress={ip}"
+    r = req.get( url, data=data, headers=headers)
+
+    dic = json.loads(r.content.decode())
+    return f"IP: {ip} malicious factor: {dic['data']['abuseConfidenceScore']}"
 
 
 def cmd(command):
@@ -18,6 +35,8 @@ def sniffer():
     count = 1
     print(cmd("ss -tp"))
     unique = []
+    s = socke.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
     for packet in capture.sniff_continuously(packet_count=500):
         try:
             if int(packet.length) > 200:
@@ -31,25 +50,23 @@ def sniffer():
 
                 if dst_addr in unique:
                     continue
-			
+
                 unique.append(dst_addr)
                 print(f"Packet Number {count}:")
                 print ("%s IP %s:%s <-> %s:%s (%s)" % (localtime, src_addr, src_port, dst_addr, dst_port, protocol),"\n")
                 print(f"Packet Length: {packet.length}")
-                if dst_addr != '192.168.0.39':
-#                   print(sp.run(["nmap", "-O", "-Pn", dst_addr], capture_output=True).stdout.decode())
-
+                if dst_addr != s.getsockname()[0]:
                     lines = cmd("netstat -na | grep -i established").split("\n")
 
                     ip_list = []
                     for line in lines[:-1]:
                         ip_list.append(line.split(" ")[-2].split(":")[0])
- 
+
                     if dst_addr in ip_list:
                         print("ip is legit")
                     else:
-                        print(f"ip: {dst_addr} looking a little sus ngl")
-
+                        print(f"IP: {dst_addr} looking a little sus ngl")
+                        print(check_ip(dest_addr))
                 print(separator)   
                 count += 1
             else:
@@ -57,6 +74,7 @@ def sniffer():
         except AttributeError as e:
             pass
     print()
+   
 def snort(file):
     print(cmd("snort -A console -q -u snort -g snort -c /etc/snort/snort.conf --daq-dir /usr/lib/daq/ --pcap-list " + file))
 def main():
@@ -65,7 +83,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
-    
-    
-
